c3e0a5e922369078bcffe407a25ec13a8b919721
Adam Korpen Ström <Korpenxd@gmail.com>
2021-05-23 16:33:34 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const num1 = parseInt(prompt('Enter first number '));
+const num2 = parseInt(prompt('Enter second number '));
+const num3 = parseInt(prompt('Enter third number '));
+
+const sum = num1 + num2 + num3;
+
+document.getElementById('sum').innerText = sum;
